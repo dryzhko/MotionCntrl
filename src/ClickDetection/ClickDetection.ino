054724ce4193f3f94f35838a83e9bcc10803b0c5
Dmytro Ryzhkov <dryzhko@gmail.com>
2017-04-23 08:07:45 -0600

    Updated Arduino and finished project

--- src/ClickDetection/ClickDetection.ino
+++ src/ClickDetection/ClickDetection.ino
@@ -1,40 +1,68 @@
 #include <Keyboard.h>
 #include <Mouse.h>
 
-//set up inputs
-const int indexFinger = A7; 
-const int middleFinger = A9; 
-const int ringFinger = A10; 
-
-const int threshold = 40;
+const int indexFinger = A7; // the piezo is connected to analog pin 9 (aka D9)
+const int middleFinger = A9; // the piezo is connected to analog pin 7 (aka D6)
+const int ringFinger = A10; // the piezo is connected to analog pin 10 (aka D10)
+//const int pinkyFinger = A11; // the piezo is connected to analog pin 11 (aka D12)
  
-void setup(){
+const int pins[] = {ringFinger, indexFinger, middleFinger};
+
+char Keys[] =   {218,'i',217};
+
+boolean currentPressed[] = {false, false, false};
+ 
+const int threshold = 40;  // threshold value to decide when the detected sound is a knock or not
+ 
+void setup()
+{
+  //while (!Serial)
+  Serial.begin(115200);
+  Serial.println("start");
   Keyboard.begin();
-  Mouse.begin();
+}
+ 
+ 
+void loop()                    
+{ 
+  for (int i=0;i<3;i++) {
+    delay(1);
+    long total1 = 0;
+    long start = millis();
+    long total = 0;
+    total =  analogRead(pins[i]);
+ 
+    // check if we are sensing that a finger is touching
+    // and that it wasnt already pressed
+    if ((total > threshold) && (! currentPressed[i])) {
+      Serial.print("Key pressed #"); Serial.print(i);
+      Serial.print(" ("); Serial.print(Keys[i]); Serial.println(")");
+      currentPressed[i] = true;
+      if(i!=1){
+      Keyboard.press(Keys[i]);
+      total = 0;
+      }
+      else{
+        Mouse.click();
+      }
+      total = 0;
+    } 
+    else if ((total <= threshold) && (currentPressed[i])) {      
+      // key was released (no touch, and it was pressed before)
+      Serial.print("Key released #"); Serial.print(i);
+      Serial.print(" ("); Serial.print(Keys[i]); Serial.println(")");
+      currentPressed[i] = false;
+      if(i!=1){
+        Keyboard.release(Keys[i]);
+        total = 0;
+      }
+      total = 0;
+      
+    }
+    total = 0;
+    
+    delay(5);
+  }
 }
 
-void loop(){
-  int clck = analogRead(indexFinger);
-  int dwn = analogRead(middleFinger);
-  int up = analogRead(ringFinger);
-
-  if(clck > threshold){
-    Mouse.click();
-    delay(500);
-  }
-
-  if(dwn > threshold){
-    Keyboard.press(217);
-    delay(1000);
-    Keyboard.release(217);
-  }
-
-  if(up > threshold){
-    Keyboard.press(218);
-    delay(1000);
-    Keyboard.release(218);
-  }
-
-  
-}
 
